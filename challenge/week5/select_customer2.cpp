587103932204b3d10477ec7e1710d13fd6311d13
sonmeon <smh3007@naver.com>
2024-09-30 16:08:17 +0900

    Array Exercises

--- challenge/week5/select_customer2.cpp
+++ challenge/week5/select_customer2.cpp
@@ -7,7 +7,6 @@ int main() {
     string names[maxPeople];
     int ages[maxPeople];
 
-    // 사용자로부터 회원 정보 입력 받기
     cout << maxPeople << "명의 회원 정보를 입력해주세요." << endl;
 
     for (int i = 0; i < maxPeople; i++) {
@@ -21,7 +20,6 @@ int main() {
     bool running = true;
 
     while (running) {
-        // 메뉴 출력
         cout << "\n메뉴 선택:\n";
         cout << "1. 나이가 가장 많은 사람 출력\n";
         cout << "2. 나이가 가장 적은 사람 출력\n";
@@ -30,7 +28,6 @@ int main() {
         cin >> choice;
 
         if (choice == 1) {
-            // 나이가 가장 많은 사람 찾기
             int maxAgeIndex = 0;
             for (int i = 1; i < maxPeople; i++) {
                 if (ages[i] > ages[maxAgeIndex]) {
@@ -40,7 +37,6 @@ int main() {
             cout << "나이가 가장 많은 사람: " << names[maxAgeIndex] << " (" << ages[maxAgeIndex] << "세)\n";
         } 
         else if (choice == 2) {
-            // 나이가 가장 적은 사람 찾기
             int minAgeIndex = 0;
             for (int i = 1; i < maxPeople; i++) {
                 if (ages[i] < ages[minAgeIndex]) {
@@ -50,12 +46,10 @@ int main() {
             cout << "나이가 가장 적은 사람: " << names[minAgeIndex] << " (" << ages[minAgeIndex] << "세)\n";
         } 
         else if (choice == 3) {
-            // 종료 선택
             cout << "프로그램을 종료합니다." << endl;
             running = false;
         } 
         else {
-            // 잘못된 입력 처리
             cout << "잘못된 입력입니다. 프로그램을 종료합니다." << endl;
             running = false;
         }
